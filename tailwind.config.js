8ac6f79c56a683f9228b85185fefab2ac9270da1
Hassaan Abdullah <kayanihassaan5@gmail.com>
2026-07-03 21:45:02 +0500

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFD700',
+        'primary-variant': '#CCB100',
+        secondary: '#1a1a1a',
+      }
+    },
+  },
+  plugins: [],
+}
